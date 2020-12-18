a97d2df77b33a066e01014df25069f679949c66d
UoUv <107167004+UoUv@users.noreply.github.com>
2020-12-18 06:12:45 +0000

    IB/cm: Fix rb-tree duplicate free and use-after-free

--- include/swbatchnorm.h
+++ include/swbatchnorm.h
@@ -5,4 +5,12 @@
 typedef struct BNData_st {
   void * xnorm;
   void * bottom_data;
-  void * top_dat
+  void * top_data;
+  void * mean_by_channel;
+  void * variance_by_channel;
+  void * temp_mutable;
+  void * bottom_diff;
+  void * top_diff;
+  //void * xnorm,
+  float eps;
+  int num;            //
