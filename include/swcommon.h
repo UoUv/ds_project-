752170944ac59613e7e9a910875b9f92b73d0d36
UoUv <107167004+UoUv@users.noreply.github.com>
2020-12-24 16:28:42 +0000

    README update.

--- include/swcommon.h
+++ include/swcommon.h
@@ -3,3 +3,9 @@
 #include <athread.h>
 #include <sys/time.h>
 
+void start_use_slave();
+
+void end_use_slave();
+
+#define TIME(a,b) (1.0*((b).tv_sec-(a).tv_sec)+0.000001*((b).tv_usec-(a).tv_usec))
+#endif
