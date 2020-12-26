7a4e734c22aabe792d5e0ba63d801dca894c29e5
UoUv <107167004+UoUv@users.noreply.github.com>
2020-12-26 13:12:40 +0000

    updated download link

--- include/swtensortrans.h
+++ include/swtensortrans.h
@@ -1,4 +1,14 @@
 #ifndef MATRIX_TRANS_H_
 #define MATRIX_TRANS_H_
 
-//#include <
+//#include <simd.h>
+
+#define HWSIZE       48
+#define BSIZE        64
+#define NUM_THREADS 64
+#define BUFFS       3072 //(24*1024) 
+
+typedef struct _tagSlaveParam
+{
+	int B,N,H,W,splitNB,splitHW;
+	int nCount,nBNThread
