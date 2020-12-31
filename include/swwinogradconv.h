891ca4e60fbeacf90d08abbea81b50ce9f747be6
UoUv <107167004+UoUv@users.noreply.github.com>
2020-12-31 15:36:33 +0000

    dfa

--- include/swwinogradconv.h
+++ include/swwinogradconv.h
@@ -6,4 +6,13 @@
 #include <malloc.h>
 
 //#define Ni 64 
-//#defin
+//#define Ri 32 
+//#define Ci 32 
+//#define No 64  //hard coded for now
+//#define K 4 
+//#define Ro (Ri-K+1)
+//#define Co (Ci-K+1)
+////#define vB 4
+//#define B 32 
+
+typedef struct Inpu
