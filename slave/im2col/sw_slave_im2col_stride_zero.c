d5af9e9fa25c6f2945c129c04f7e3784d403d79b
UoUv <107167004+UoUv@users.noreply.github.com>
2021-01-05 12:45:14 +0000

    mempolicy: fix a race in shared_policy_replace()

--- slave/im2col/sw_slave_im2col_stride_zero.c
+++ slave/im2col/sw_slave_im2col_stride_zero.c
@@ -1,4 +1,10 @@
 #include "slave.h"
 #include "simd.h"
 #include "dma.h"
-#include "include/
+#include "include/swim2col.h"
+
+/*******
+ * col (Ni*K*K, Co*Ro)
+ * make Ni*K*K is 8x, fortunately we do not align Co*Ro*Batch
+ * make Co*Ro is 128x
+ * read batch_size input image rows to increase avai
