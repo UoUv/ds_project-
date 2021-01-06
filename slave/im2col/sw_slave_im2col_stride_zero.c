947fad3b2b63e7fefeffef3cb54baad688d70e34
UoUv <107167004+UoUv@users.noreply.github.com>
2021-01-06 05:31:07 +0000

    Prepare 1.7.1 release

--- slave/im2col/sw_slave_im2col_stride_zero.c
+++ slave/im2col/sw_slave_im2col_stride_zero.c
@@ -7,4 +7,11 @@
  * col (Ni*K*K, Co*Ro)
  * make Ni*K*K is 8x, fortunately we do not align Co*Ro*Batch
  * make Co*Ro is 128x
- * read batch_size input image rows to increase avai
+ * read batch_size input image rows to increase avaiable Bandwidth
+ * put a batch of data
+ ******/
+
+void sw_im2col_large_stride_zeropad_f(Im2colPara *para) {
+  int i, j;
+  dma_desc dma_get_im, dma_put_col;
+#define Type fl
