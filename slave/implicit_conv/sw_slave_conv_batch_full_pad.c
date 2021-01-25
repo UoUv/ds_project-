6742b20f84440acdf08e9286d0b687f0e9fdb146
UoUv <107167004+UoUv@users.noreply.github.com>
2021-01-25 05:48:42 +0000

    Update creating-group.md

--- slave/implicit_conv/sw_slave_conv_batch_full_pad.c
+++ slave/implicit_conv/sw_slave_conv_batch_full_pad.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h
+#include <unistd.h>
+#include <slave.h>
+#include <math.h>
+#include "simd.h"
+#include "dma.h"
+#include "./include/sw_conv_implicit.h"
+
+/***************
+ * GEMM PLAN 
+ * Jerry Fang 
+ * 2017 June 18
+ *
+ * input  i
