d74d2eb4c2ae7a342682fdaa78f1f31ed51d139d
UoUv <107167004+UoUv@users.noreply.github.com>
2021-02-25 17:27:03 +0000

    channel

--- slave/implicit_conv/sw_slave_conv_batch_full_pad.c
+++ slave/implicit_conv/sw_slave_conv_batch_full_pad.c
@@ -164,4 +164,18 @@ void conv_full_pad(ConvData* param)
 				        (Type*)(local_weight),
 				        (Type*)(local_output + (cCo-CoStart)*No*B/64/SIMDSIZE),
 				        B/8/4,
-		
+				        B/8/4,
+				        No/8,
+				        Ni/8,
+				        rid,
+				        cid
+              );
+			      }//if
+          }//cKc
+      }//cCi
+
+    }//cKc
+
+    //input back outer
+    jj=0;
+   
