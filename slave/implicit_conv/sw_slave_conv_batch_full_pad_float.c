288b5008c570ee6e6036ed4f13ac6c6cdd750e31
UoUv <107167004+UoUv@users.noreply.github.com>
2021-02-28 17:51:06 +0000

    Updated notebooks

--- slave/implicit_conv/sw_slave_conv_batch_full_pad_float.c
+++ slave/implicit_conv/sw_slave_conv_batch_full_pad_float.c
@@ -5,3 +5,13 @@
 #include <math.h>
 #include "simd.h"
 #include "dma.h"
+#include "../include/sw_conv_implicit.h"
+
+/***************
+ * GEMM PLAN 
+ * Jerry Fang 
+ * 2017 June 18
+ *
+ * input  is of dim(B, Ni)
+ * weight is of dim(Ni, No)
+ * ouput  is of d
