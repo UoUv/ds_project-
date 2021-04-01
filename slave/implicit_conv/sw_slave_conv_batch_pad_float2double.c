dd440e4e02f2a6140c24abb448e48823e790cd51
UoUv <107167004+UoUv@users.noreply.github.com>
2021-04-01 14:52:27 +0000

    fix preview

--- slave/implicit_conv/sw_slave_conv_batch_pad_float2double.c
+++ slave/implicit_conv/sw_slave_conv_batch_pad_float2double.c
@@ -4,4 +4,14 @@
  * 2017 June 15 
  *
  * input  is of dim(B, Ni)
- * weight is of dim(Ni, No
+ * weight is of dim(Ni, No)
+ * ouput  is of dim(B, No)
+ *
+ * No overlap input DMA and weight DMA
+ * ************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <slave.h>
+#i
