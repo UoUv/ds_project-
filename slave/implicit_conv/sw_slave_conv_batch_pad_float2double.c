50252364a5ca294c0fa48a1743e8dffd649b448d
UoUv <107167004+UoUv@users.noreply.github.com>
2021-04-09 22:25:09 +0000

    Added debug code

--- slave/implicit_conv/sw_slave_conv_batch_pad_float2double.c
+++ slave/implicit_conv/sw_slave_conv_batch_pad_float2double.c
@@ -144,4 +144,9 @@ void conv_pad_float__(ConvData* param)
             //fjrpad
             int lc = cCi - pad;
             if(!(lc >= 0 && lc < Ci))
-                cont
+                continue;
+
+    			  dma(dma_get_input, (long)(input_start + (lc+lr*Ci)*Ni*B), (long)(local_input));
+    			  dma_wait(&input_replyget, 1); input_replyget = 0;
+
+            for(i=local_input_size*SI
