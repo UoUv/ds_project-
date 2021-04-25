1f7dea7baec65bc2586c136edae1243d2e0a165f
UoUv <107167004+UoUv@users.noreply.github.com>
2021-04-25 09:44:36 +0000

    Add contributing.md

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -28,4 +28,7 @@ void poolingBackwardMax(SlavePoolingParam *pParam)
 {
   const int nMaxBuffSize = 49152;//58KB 
 	int pooled_height_,pooled_width_,stride_h_,stride_w_,pad_h_,pad_w_,kernel_h_,kernel_w_,height_,width_;
-	int nCount,nMaxThreadsNum,nLe
+	int nCount,nMaxThreadsNum,nLeftMaxThreadsNum,nOffset,nOffset0,nOffset1;
+	int nBottomOffset,nTopOffset,use_top_mask;
+	int ph,pw,hstart,hend,wstart,wend,pool_index,h,w,index,bottom_index;
+	Type *pTopData,*pBottom
