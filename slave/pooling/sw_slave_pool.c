32ba1432bf4e090ab0a55119abd7b34d84bb24ea
UoUv <107167004+UoUv@users.noreply.github.com>
2021-04-30 08:00:29 +0000

    Fixed possible NPE

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -77,4 +77,10 @@ void poolingBackwardMax(SlavePoolingParam *pParam)
 		int nSplitCount=0,nSplitRows =0,nLeftRows = 0;
 		int nTopSize1 = 0,nMaskSize1=0,nBottomIndex;
 		nSplitRows = pooled_height_;
-        int nKernelSize 
+        int nKernelSize = kernel_h_ *width_*sizeof(Type);
+        int nStartAddr = 0;
+		while(nBottomSize <nKernelSize)
+		{
+			nSplitRows = nSplitRows>>1;			
+			nTopSize = nSplitRows*pooled_width_*sizeof(Type);
+			if(use_top_mask 
