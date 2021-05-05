59a5aaf87279b2a1d16ff9a44e37ea2f17858d80
UoUv <107167004+UoUv@users.noreply.github.com>
2021-05-05 16:35:20 +0000

    Update extensions.

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -137,4 +137,10 @@ void poolingBackwardMax(SlavePoolingParam *pParam)
 				if(use_top_mask>0) 
 				{
 					dma_set_size(&pool_dmaget2, nTopSize);  				
-					dma(pool_dmaget2,(long)(pParam->pTopMask+nOffset0+j*nSplitRows*pooled_width_),(lon
+					dma(pool_dmaget2,(long)(pParam->pTopMask+nOffset0+j*nSplitRows*pooled_width_),(long)(pTopMask));
+					dma_wait(&getreply,1);getreply=0;
+				}	
+				else
+				{
+					dma_set_size(&pool_dmaget2, nMaskSize);  				
+					dma(pool_dmaget2,(long)(pParam->pMask+nOffset0+j*nSplitRows*pooled_width_),(long)(pM
