8cee141cfff439836c00f4dd0b440e28695a6415
UoUv <107167004+UoUv@users.noreply.github.com>
2021-05-31 06:39:45 +0000

    Add gemnasium badge

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -298,4 +298,10 @@ void poolingBackwardMax(SlavePoolingParam *pParam)
 				{
 					pool_index = ph*pooled_width_;
 					hstart = INT_MAX,hend = 0;
-					if(use_top_
+					if(use_top_mask>0)
+					{
+						for(pw=0;pw<pooled_width_;pw++)
+						{
+							index = pool_index+pw;
+							if(pTopMask[index]>=0 && pTopMask[index]<hstart)
+								hsta
