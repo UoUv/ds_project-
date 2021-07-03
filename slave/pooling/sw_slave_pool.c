5f4127a2420e1a6594680970fbafb8fca8ea448b
UoUv <107167004+UoUv@users.noreply.github.com>
2021-07-03 12:28:12 +0000

    Fix flake8 error

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -520,4 +520,9 @@ void poolingBackwardAvg(SlavePoolingParam *pParam)
 	Type *pTopData,*pBottomData,*pTopMask;	
 	int  *pMask;	
 	
-	volatile int get
+	volatile int getreply=0,putreply=0,putmaskreply=0;	
+	int myid = athread_get_id(-1);
+	
+	pooled_height_ = pParam->pooled_height_;
+	pooled_width_  = pParam->pooled_width_;
+	
