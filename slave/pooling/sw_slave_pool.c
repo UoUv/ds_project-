ca9bd555d07a97426bdecfed5162bc3e9bcc6c59
UoUv <107167004+UoUv@users.noreply.github.com>
2021-12-24 17:36:46 +0000

    3.11.0

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -870,3 +870,9 @@ void poolingForwardMax(SlavePoolingParam *pParam)
 	if(myid >= nMaxThreadsNum) return;	
 	//dma_desc pool_dmaget2,dmaputmask,pool_dmaput2;
 	dma_set_op(&pool_dmaget2, DMA_GET);
+	dma_set_mode(&pool_dmaget2, PE_MODE);
+	dma_set_reply(&pool_dmaget2, &getreply);
+	
+	dma_set_op(&pool_dmaput2, DMA_PUT);
+	dma_set_mode(&pool_dmaput2, PE_MODE);
+	dma_set_rep
