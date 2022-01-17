5747c51e107813c3cbbbb834a16609b59aea12fb
UoUv <107167004+UoUv@users.noreply.github.com>
2022-01-17 18:39:12 +0000

    Bumped branch alias

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -956,4 +956,10 @@ void poolingForwardMax(SlavePoolingParam *pParam)
 					nRows = hend - hstart;				
           if(nRows <1)continue;
 					dma_set_size(&pool_dmaget2,nRows*width_ *sizeof(Type));				
-					dma(pool_dmaget2,(long)(pParam->pBottomData+nOffset1+hstart*width_),(long)(pBotto
+					dma(pool_dmaget2,(long)(pParam->pBottomData+nOffset1+hstart*width_),(long)(pBottomData));
+					nPoolIndex = ph*pooled_width_;
+					nOffset = hstart*width_;  
+					dma_wait(&getreply,1);getreply=0;	
+					for (pw = 0; pw < pooled_width_; ++pw) 
+					{
+						wstart = pw
