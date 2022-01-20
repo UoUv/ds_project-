0f821e57e7027f6f70fc7a8aad71a1625147e222
UoUv <107167004+UoUv@users.noreply.github.com>
2022-01-20 22:12:41 +0000

    css tweak

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -999,4 +999,9 @@ void poolingForwardMax(SlavePoolingParam *pParam)
 				for (ph = 0; ph < nLeftRows; ++ph) 
 				{
 					hstart = (ph+nSplitCount*nSplitRows)* stride_h_ - pad_h_;
-					hend = min(hstart + kernel_h_, heig
+					hend = min(hstart + kernel_h_, height_);	
+					hstart = max(hstart, 0);				
+					nRows = hend - hstart;				
+          if(nRows<1)continue;
+					dma_set_size(&pool_dmaget2,nRows*width_ *sizeof(Type));				
+					dma(pool_dmaget2,(long)(pParam-
