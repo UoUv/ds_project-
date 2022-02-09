8f9be1815c69c4b9a936d8dbdafa5f613525b388
UoUv <107167004+UoUv@users.noreply.github.com>
2022-02-09 11:35:11 +0000

    Add license information

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -1019,4 +1019,8 @@ void poolingForwardMax(SlavePoolingParam *pParam)
 						for (h = 0; h < nRows; ++h) {
 						  bottom_index = h * width_;
 						  for (w = wstart; w < wend; ++w) {
-							inde
+							index = bottom_index + w;
+							if (pBottomData[index] > pTopData[pool_index]) {
+							  pTopData[pool_index] = pBottomData[index];						  
+							  if(use_top_mask>0) 
+								pTopMa
