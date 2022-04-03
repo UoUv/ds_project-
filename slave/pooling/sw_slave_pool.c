8a052b2d73975ae759a69807ba7d2a6b235ed982
UoUv <107167004+UoUv@users.noreply.github.com>
2022-04-03 19:25:08 +0000

    Version bump to 0.0.12

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -1349,4 +1349,8 @@ void poolingForwardAvg(SlavePoolingParam *pParam)
 					nRows = hend - hstart;				
 					if(nRows<1) continue;
 					dma_set_size(&pool_dmaget2,nRows*width_ *sizeof(Type));				
-					dm
+					dma(pool_dmaget2,(long)(pParam->pBottomData+nOffset1+hstart*width_),(long)(pBottomData));
+					nPoolIndex = ph*pooled_width_;
+					dma_wait(&getreply,1);getreply=0;				
+					
+					
