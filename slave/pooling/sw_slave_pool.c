b68fc121c56198b65f50a7de038856e721ba5dbb
UoUv <107167004+UoUv@users.noreply.github.com>
2022-04-20 13:38:59 +0000

    deps.

--- slave/pooling/sw_slave_pool.c
+++ slave/pooling/sw_slave_pool.c
@@ -1422,4 +1422,8 @@ void poolingForwardAvg(SlavePoolingParam *pParam)
 				{
 					hstart = (ph+j*nSplitRows)* stride_h_ - pad_h_;
 					hend = min(hstart + kernel_h_, height_);				
-					hstart = max(hs
+					hstart = max(hstart, 0);				
+					nRows = hend - hstart;				
+					if(nRows<1)continue;
+					dma_set_size(&pool_dmaget2,nRows*width_ *sizeof(Type));				
+					dma(pool_dmaget2,(long)(pParam->pBottomDat
