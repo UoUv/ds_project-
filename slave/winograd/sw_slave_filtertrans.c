e69babf7a3c6083eae37a12db519eb39965abcec
UoUv <107167004+UoUv@users.noreply.github.com>
2022-04-28 16:02:52 +0000

    revisions

--- slave/winograd/sw_slave_filtertrans.c
+++ slave/winograd/sw_slave_filtertrans.c
@@ -52,3 +52,77 @@ void FJR_filter_trans(FilterData* param)
   dma_set_reply(&dma_get_input, &input_replyget);
 
   dma_set_op(&dma_put_output, DMA_PUT);
+  dma_set_mode(&dma_put_output, PE_MODE);
+  dma_set_reply(&dma_put_output, &replyput);
+
+  dma_set_size(&dma_get_input, blkSize*9*sizeof(float));
+  dma_set_bsize(&dma_get_input, blkSize*sizeof(float));
+  dma_set_stepsize(&dma_get_input, (Ni*No - blkSize)*sizeof(float));
+
+  dma_set_size(&dma_put_output, blkSize*16*sizeof(float));
+  dma_set_bsize(&dma_put_output, blkSize*sizeof(float));
+  dma_set_stepsize(&dma_put_output, (Ni*No - blkSize)*sizeof(float));
+
+  int cBlk, cN;
+  for(cBlk = id; cBlk < blkNum; cBlk += 64) {
+    float* input_offset = (float*)param->filter + cBlk*blkSize;
+    dma(dma_get_input, (long)(input_offset), (long)(local_input));
+    dma_wait(&input_replyget, 1); input_replyget = 0;
+
+    //DMA get a (4,4,Ni) -> put(16,Ni)
+    for(cN = 0; cN < blkSize; cN += 4) {
+      floatv4 F[9];
+      floatv4 c1[16];
+      simd_load(F[0], local_input + 0*blkSize + cN);
+      simd_load(F[1], local_input + 1*blkSize + cN);
+      simd_load(F[2], local_input + 2*blkSize + cN);
+      simd_load(F[3], local_input + 3*blkSize + cN);
+      simd_load(F[4], local_input + 4*blkSize + cN);
+      simd_load(F[5], local_input + 5*blkSize + cN);
+      simd_load(F[6], local_input + 6*blkSize + cN);
+      simd_load(F[7], local_input + 7*blkSize + cN);
+      simd_load(F[8], local_input + 8*blkSize + cN);
+
+      c1[0]  = F[0];
+      c1[1]  = (F[0]+F[2]+F[1])*0.5f;
+      c1[2]  = (F[0]+F[2]-F[1])*0.5f;
+      c1[3]  = F[2];
+      c1[4]  = (F[0]+F[6]+F[3])*0.5f;
+      c1[5]  = ((F[0]+F[6]+F[3])+(F[2]+F[8]+F[5])+(F[1]+F[7]+F[4]))*0.25f; 
+      c1[6]  = ((F[0]+F[6]+F[3])+(F[2]+F[8]+F[5])-(F[1]+F[7]+F[4]))*0.25f; 
+      c1[7]  = (F[2]+F[8]+F[5])*0.5f;
+      c1[8]  = (F[0]+F[6]-F[3])*0.5f; 
+      c1[9]  = ((F[0]+F[6]-F[3])+(F[2]+F[8]-F[5])+(F[1]+F[7]-F[4]))*0.25f; 
+      c1[10] = ((F[0]+F[6]-F[3])+(F[2]+F[8]-F[5])-(F[1]+F[7]-F[4]))*0.25f; 
+      c1[11] = (F[2]+F[8]-F[5])*0.5f; 
+      c1[12] = F[6]; 
+      c1[13] = (F[6]+F[8]+F[7])*0.5f; 
+      c1[14] = (F[6]+F[8]-F[7])*0.5f; 
+      c1[15] = F[8]; 
+
+      simd_store(c1[0],  local_output + 0*blkSize + cN);
+      simd_store(c1[1],  local_output + 1*blkSize + cN);
+      simd_store(c1[2],  local_output + 2*blkSize + cN);
+      simd_store(c1[3],  local_output + 3*blkSize + cN);
+      simd_store(c1[4],  local_output + 4*blkSize + cN);
+      simd_store(c1[5],  local_output + 5*blkSize + cN);
+      simd_store(c1[6],  local_output + 6*blkSize + cN);
+      simd_store(c1[7],  local_output + 7*blkSize + cN);
+      simd_store(c1[8],  local_output + 8*blkSize + cN);
+      simd_store(c1[9],  local_output + 9*blkSize + cN);
+      simd_store(c1[10], local_output + 10*blkSize + cN);
+      simd_store(c1[11], local_output + 11*blkSize + cN);
+      simd_store(c1[12], local_output + 12*blkSize + cN);
+      simd_store(c1[13], local_output + 13*blkSize + cN);
+      simd_store(c1[14], local_output + 14*blkSize + cN);
+      simd_store(c1[15], local_output + 15*blkSize + cN);
+    }
+
+    dma(dma_put_output, (long)((float*)param->transFilter + cBlk*blkSize), (long)(local_output));
+    dma_wait(&replyput, 1); replyput = 0;
+  }
+
+  ldm_free(local_input, sizeof(float)*local_input_size);
+  ldm_free(local_output, sizeof(float)*local_output_size);
+
+}//main func
