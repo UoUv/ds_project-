322e9b5b95a7ca20460e782453b0c0ae57047bbc
UoUv <107167004+UoUv@users.noreply.github.com>
2022-04-28 17:53:05 +0000

    for

--- slave/winograd/sw_slave_inputtrans_Ni512.c
+++ slave/winograd/sw_slave_inputtrans_Ni512.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
-#include 
+#include <stdlib.h>
+#include <unistd.h>
+#include <slave.h>
+#include <math.h>
+#include <dma.h>
+#include "./include/swwinogradconv.h"
+
+/***************
+ * GEMM PLAN 
+ * Jerry Fang 
+ * 2018.Sep.19th
+ *
+ 
