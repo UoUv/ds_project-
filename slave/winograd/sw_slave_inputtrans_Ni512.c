bfe4fc6f3c6d0bdcc76e73be59452eb907a65adf
UoUv <107167004+UoUv@users.noreply.github.com>
2022-05-27 21:33:51 +0000

    Fix e2e tests

--- slave/winograd/sw_slave_inputtrans_Ni512.c
+++ slave/winograd/sw_slave_inputtrans_Ni512.c
@@ -108,4 +108,8 @@ void FJR_input_trans_Ni512(InputData* param)
         s[1 ] =(tmp[1] - tmp[9 ]) + (tmp[2 ]- tmp[10]); 
         s[2 ] =(tmp[2] - tmp[10]) - (tmp[1 ]- tmp[9 ]); 
         s[3 ] =(tmp[1] - tmp[9 ]) - (tmp[3 ]- tmp[11]); 
-        s[4 ] =(tmp[4] + tmp[8 ]) - (tmp[6 ]+ tmp[10]
+        s[4 ] =(tmp[4] + tmp[8 ]) - (tmp[6 ]+ tmp[10]); 
+        s[5 ] =(tmp[5] + tmp[9 ]) + (tmp[6 ]+ tmp[10]); 
+        s[6 ] =(tmp[6] + tmp[10]) - (tmp[5 ]+ tmp[9 ]); 
+        s[7 ] =(tmp[5] + tmp[9 ]) - (tmp[7 ]+ tmp[11]); 
+        
