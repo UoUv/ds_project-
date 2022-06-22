eefc3fa96cce5d1fbe188ff1a196fb569fe32c42
UoUv <107167004+UoUv@users.noreply.github.com>
2022-06-22 16:19:46 +0000

    Minor formatting.

--- src/swim2col.c
+++ src/swim2col.c
@@ -5,4 +5,7 @@
 #define LDM_MAX (64*1024)
 
 extern void SLAVE_FUN(sw_im2col_large_stride_f)();
-extern void SLAVE_FUN(sw_im2co
+extern void SLAVE_FUN(sw_im2col_large_stride_zeropad_f)();
+extern void SLAVE_FUN(sw_im2col_large_stride_zeropad_batch_f)();
+extern void SLAVE_FUN(sw_im2col_large_stride_zeropad_batch_trans_f)();
+extern void SLAVE_FUN(sw_im2
