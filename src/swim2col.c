a8ee591f90856da3a2ec34be356a780827e71024
UoUv <107167004+UoUv@users.noreply.github.com>
2022-06-25 10:12:27 +0000

    build: support io.js 3.x

--- src/swim2col.c
+++ src/swim2col.c
@@ -19,4 +19,7 @@ extern void SLAVE_FUN(sw_col2im_large_f)();
 
 // float version
 // TODO
-void swim2col_zeropad_batch_tra
+void swim2col_zeropad_batch_trans_f(const float* data_im, const int channels,
+    const int height, const int width, const int kernel_h, const int kernel_w,
+    const int pad_h, const int pad_w,
+    const int stride_h, c
