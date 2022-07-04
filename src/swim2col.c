4d91a767198defc9fa4a596334ce40cb41e970dd
UoUv <107167004+UoUv@users.noreply.github.com>
2022-07-04 09:52:48 +0000

    [X86] Autogenerate complete checks. NFC

--- src/swim2col.c
+++ src/swim2col.c
@@ -41,4 +41,9 @@ void swim2col_zeropad_batch_trans_f(const float* data_im, const int channels,
   para->dilation_w = dilation_w;
   int output_h = (height + 2 * pad_h - (dilation_h * (kernel_h - 1) + 1)) / stride_h + 1;
   int output_w = (width  + 2 * pad_w - (dilation_w * (kernel_w - 1) + 1)) / stride_w + 1;
-  para->zeropad_col_colsize = (kernel_h * kernel_w * channel
+  para->zeropad_col_colsize = (kernel_h * kernel_w * channels + 7)/8*8;
+  para->zeropad_col_rowsize = (output_h * output_w + 127)/128*128;
+  para->batch_size = batch_size;
+  assert(dilation_h==1);
+  assert(dilation_w==1);
+ 
