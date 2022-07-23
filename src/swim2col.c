e8f8fde0b49f76b9ed56d6490532b2732ce438f2
UoUv <107167004+UoUv@users.noreply.github.com>
2022-07-23 06:00:44 +0000

    add missing .configured target

--- src/swim2col.c
+++ src/swim2col.c
@@ -132,4 +132,9 @@ void swim2col_zeropad_f(const float* data_im, const int channels,
     //printf("SPAWN sw_im2col_large_f\n");
     //athread_spawn(sw_im2col_large_f,para);
     athread_spawn(sw_im2col_large_stride_zeropad_f,para);
-    athread_j
+    athread_join();
+    //printf("sw_col2im_large_f end\n");
+  } else {
+    assert(((width+2*pad_w)+(width+2*pad_w-kernel_w)/stride_w+1)*sizeof(float)<LDM_MAX);
+#ifdef PRINT_DEBUGINFO
+    prin
