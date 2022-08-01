b307edfcc54d424b230fc76ed3be3527520c88b4
UoUv <107167004+UoUv@users.noreply.github.com>
2022-08-01 14:31:25 +0000

    Removed trailing white space at end of lines

--- src/swlstm.c
+++ src/swlstm.c
@@ -6,4 +6,10 @@
 #include "../include/swlstm.h"
 
 extern SLAVE_FUN(lstm_slave_clip_forward_f)();
-extern SLAVE_FUN(lstm_slave_noclip_forward_f)
+extern SLAVE_FUN(lstm_slave_noclip_forward_f)();
+extern SLAVE_FUN(lstm_std_slave_forward_f)();
+void sw_lstm_clip_forward_impl_f(
+        float * clip_t,
+        float * pre_gate_t,
+        float * h_to_gate,
+        float * ga
