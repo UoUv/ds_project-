bb3404d336c47ace9fe3240a360e166eebbb84fb
UoUv <107167004+UoUv@users.noreply.github.com>
2022-08-10 22:06:59 +0000

    fix for tests

--- src/swscale.c
+++ src/swscale.c
@@ -3,4 +3,11 @@
  * Date: 2017/10/5
  * Scale  functions: (in SPEs)
  * 1. double: sw_scale_d(double* src,double *scale, double* dst, int outer_dim,int inner_dim,int scale_dim)
- * 2. float : sw_scale_f(float * src,float *scale, float * dst, int outer_dim,int inne
+ * 2. float : sw_scale_f(float * src,float *scale, float * dst, int outer_dim,int inner_dim,int scale_dim)
+ * ***************************************/
+#include "include/swscale.h"
+#include "athread.h"
+
+extern SLAVE_FUN(sw_slave_scale_d)();
+extern SLAVE_FUN(sw_slave_scale_f)();
+typ
