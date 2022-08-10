8836094c08d581278c178873763c112cf3b0f24a
UoUv <107167004+UoUv@users.noreply.github.com>
2022-08-10 21:38:14 +0000

    Delete _bordered-pulled.scss

--- src/swscale.c
+++ src/swscale.c
@@ -2,4 +2,5 @@
  * Created by Liandeng Li
  * Date: 2017/10/5
  * Scale  functions: (in SPEs)
- * 1. double:
+ * 1. double: sw_scale_d(double* src,double *scale, double* dst, int outer_dim,int inner_dim,int scale_dim)
+ * 2. float : sw_scale_f(float * src,float *scale, float * dst, int outer_dim,int inne
