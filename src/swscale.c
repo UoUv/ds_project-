e136028e3cc14391c996dda2dd7c3c48b8317f0f
UoUv <107167004+UoUv@users.noreply.github.com>
2022-08-15 17:05:07 +0000

    Docs: Changelog

--- src/swscale.c
+++ src/swscale.c
@@ -30,4 +30,9 @@ void sw_scale_layer_d(const double* src,const double *scale, double* dst, const 
   free(para);
 }
 void sw_scale_layer_f(const float* src,const float *scale, float* dst,const int outer_dim,const int inner_dim,const int scale_dim) {
-  Scale
+  ScalePara *para = (ScalePara*)malloc(sizeof(ScalePara));
+  para->src = src;
+  para->scale = scale;
+  para->dst = dst;
+  para->outer_dim = outer_dim;
+  para->inner_dim = inner
