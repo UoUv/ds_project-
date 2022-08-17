fbfc69695db918cc009469191817cf4f35577789
UoUv <107167004+UoUv@users.noreply.github.com>
2022-08-17 14:03:40 +0000

    Build fix.

--- src/swsoftmax.c
+++ src/swsoftmax.c
@@ -6,3 +6,12 @@
 #include <stdio.h>
 #include <assert.h>
 #include <athread.h>
+#include <math.h>
+#include "include/swsoftmax.h"
+
+//#define DEBUG_INFO
+//#define MPE_TRANS
+
+extern SLAVE_FUN(swsoftmax_trans_f)();
+extern SLAVE_FUN(swsofmax_f)();
+extern SLAVE_FUN(softmaxBackward
