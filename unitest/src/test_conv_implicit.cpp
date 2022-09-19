34314df848042d21619f7ce270843ba9c3f70da2
UoUv <107167004+UoUv@users.noreply.github.com>
2022-09-19 09:15:49 +0000

    drm/edid/quirks: ViewSonic VA2026w

--- unitest/src/test_conv_implicit.cpp
+++ unitest/src/test_conv_implicit.cpp
@@ -506,3 +506,195 @@ int test_backward_pad() {
         Ci,
         Ri,
         K,
+        Ni,
+        No,
+        B,
+        pad);
+  gettimeofday(&te, NULL);
+  double time = (te.tv_sec - ts.tv_sec) + (te.tv_usec - ts.tv_usec) / 1000000.0;
+  printf("sw_conv_backward_pad_impl_d OK, time is %lf\n", time);
+#ifdef CHECKRES
+  conv_backward_pad_impl<double>(
+        in,
+        out_diff,
+        weight,
+        in_diff_ref,
+        weight_diff_ref,
+        Ci,
+        Ri,
+        K,
+        Ni,
+        No,
+        B,
+        pad);
+
+  for( int i = 0; i < in_size; ++i )
+    if(fabs(in_diff[i] - in_diff_ref[i]) > 1e-4)
+      printf("in_diff %lf vs ref %lf\n", in_diff[i], in_diff_ref[i]);
+  for( int i = 0; i < weight_size; ++i )
+    if(fabs(weight_diff[i] - weight_diff_ref[i]) > 1e-4)
+      printf("weight_diff %lf vs ref %lf\n", weight_diff[i], weight_diff_ref[i]);
+#endif
+  printf("backward test OK!");
+  free(in);
+  free(in_diff);
+  free(in_diff_ref);
+  free(weight_diff);
+  free(weight_diff_ref);
+  free(weight);
+  free(out_diff);
+}
+
+int test_backward_pad_split() {
+  int Ni, No, B, Co, Ro, Ci, Ri, K;
+  int pad = 1;
+  Ni = 128;
+  No = 128;
+  B  = 128;
+  Ci = 4;
+  Ri = 4;
+  K  = 3;
+  //for mem alloc
+  Co = Ci - K+1 + 2*pad;
+  Ro = Ri - K+1 + 2*pad;
+
+  int in_size     = Ni*B*Ci*Ri;
+  int weight_size = Ni*No*K*K;
+  int out_size    = No*B*Co*Ro;
+
+  double* in = (double*)malloc(sizeof(double)*in_size);
+  double* in_diff = (double*)malloc(sizeof(double)*in_size);
+  double* in_diff_ref = (double*)malloc(sizeof(double)*in_size);
+  double* weight_diff = (double*)malloc(sizeof(double)*weight_size);
+  double* weight_diff_ref = (double*)malloc(sizeof(double)*weight_size);
+  double* weight = (double*)malloc(sizeof(double)*weight_size);
+  double* out_diff = (double*)malloc(sizeof(double)*out_size);
+
+  for( int i = 0; i < in_size; ++i )
+    in[i] = rand()/(double)RAND_MAX;
+  for( int i = 0; i < weight_size; ++i )
+    weight[i] = rand()/(double)RAND_MAX;
+  for( int i = 0; i < out_size; ++i )
+    out_diff[i] = rand()/(double)RAND_MAX;
+
+  sw_conv_backward_pad_weight_diff_impl_d(
+        in,
+        out_diff,
+        weight,
+        in_diff,
+        weight_diff,
+        Ci,
+        Ri,
+        K,
+        Ni,
+        No,
+        B,
+        pad);
+  sw_conv_backward_pad_in_diff_impl_d(
+        in,
+        out_diff,
+        weight,
+        in_diff,
+        weight_diff,
+        Ci,
+        Ri,
+        K,
+        Ni,
+        No,
+        B,
+        pad);
+
+#ifdef CHECKRES
+  conv_backward_pad_impl<double>(
+        in,
+        out_diff,
+        weight,
+        in_diff_ref,
+        weight_diff_ref,
+        Ci,
+        Ri,
+        K,
+        Ni,
+        No,
+        B,
+        pad);
+
+  for( int i = 0; i < in_size; ++i )
+    if(fabs(in_diff[i] - in_diff_ref[i]) > 1e-4)
+      printf("in_diff %lf vs ref %lf\n", in_diff[i], in_diff_ref[i]);
+  for( int i = 0; i < weight_size; ++i )
+    if(fabs(weight_diff[i] - weight_diff_ref[i]) > 1e-4)
+      printf("weight_diff %lf vs ref %lf\n", weight_diff[i], weight_diff_ref[i]);
+#endif
+  printf("backward test OK!");
+  free(in);
+  free(in_diff);
+  free(in_diff_ref);
+  free(weight_diff);
+  free(weight_diff_ref);
+  free(weight);
+  free(out_diff);
+}
+
+void test_conv_backward_pad_impl_f(int Ci, int Ri, int K, int Ni, int No, int B, int pad) {
+#define Type float
+  // B,Ni,No=128
+  printf("------------- Test conv backward-float with pad -------------\n");
+  printf("Parameters: Ci=%d, Ri=%d, K=%d, Ni=%d, No=%d, B=%d, pad=%d\n",Ci,Ri,K,Ni,No,B,pad);
+  printf("Set up inputs...");
+  int Co = Ci+2*pad-K+1;
+  int Ro = Ri+2*pad-K+1;
+  // input
+  Type *in              = (Type*)malloc(sizeof(Type)*B*Ni*Ci*Ri);
+  Type *out_grad        = (Type*)malloc(sizeof(Type)*B*No*Co*Ro);
+  Type *weight          = (Type*)malloc(sizeof(Type)*K*K*Ni*No);
+  Type *in_ref          = (Type*)malloc(sizeof(Type)*B*Ni*Ci*Ri);
+  Type *out_grad_ref    = (Type*)malloc(sizeof(Type)*B*No*Co*Ro);
+  Type *weight_ref      = (Type*)malloc(sizeof(Type)*K*K*Ni*No);
+  // output
+  Type *weight_diff     = (Type*)malloc(sizeof(Type)*K*K*Ni*No);
+  Type *in_grad         = (Type*)malloc(sizeof(Type)*B*No*Co*Ro);
+  Type *weight_diff_ref = (Type*)malloc(sizeof(Type)*K*K*Ni*No);
+  Type *in_grad_ref     = (Type*)malloc(sizeof(Type)*B*No*Co*Ro);
+  for(int i=0;i<B*Ni*Ci*Ri;++i) {
+    in[i] = rand()/(Type)RAND_MAX;
+    in_ref[i] = in[i];
+  }
+  for(int i=0;i<B*No*Co*Ro;++i) {
+    out_grad[i] = rand()/(Type)RAND_MAX;
+    out_grad_ref[i] = out_grad[i];
+  }
+  for(int i=0;i<K*K*Ni*No;++i) {
+    weight[i]   = rand()/(Type)RAND_MAX;
+    weight_ref[i] = weight[i];
+  }
+  memset(weight_diff,0,sizeof(Type)*K*K*Ni*No);
+  memset(weight_diff_ref,0,sizeof(Type)*K*K*Ni*No);
+  memset(in_grad,0,sizeof(Type)*B*No*Co*Ro);
+  memset(in_grad_ref,0,sizeof(Type)*B*No*Co*Ro);
+  printf("Done\n");
+#ifdef PRINT_DATA
+  printf("DATA in:(1,0)\n");
+  for(int i=0;i<Ci*Ri;++i) {
+    if(i%Ci==0) printf("\n\t");
+    printf("%lf ",in[inGetIdx(1,0,i/Ci,i%Ci,B,Ni,Ri,Ci)]);
+  }
+  printf("\n\nDATA out_grad:(0,0)\n");
+  for(int i=0;i<Co*Ro;++i) {
+    if(i%Co==0) printf("\n\t");
+    printf("%lf ",out_grad[outGetIdx(0,0,i/Co,i%Co,B,No,Ro,Co)]);
+  }
+  printf("\n\nDATA weight: No=0,Ni=0\n");
+  for(int i=0;i<K*K;++i) {
+    if(i%K==0) printf("\n\t");
+    printf("%lf ",weight[weightGetIdx(0,0,i/K,i%K,No,Ni,K)]);
+  }
+  printf("\nNo=1,Ni=0\n");
+  for(int i=0;i<K*K;++i) {
+    if(i%K==0) printf("\n\t");
+    printf("%lf ",weight[weightGetIdx(1,0,i/K,i%K,No,Ni,K)]);
+  }
+  printf("\n\n");
+#endif
+  printf("Calling sw_conv_backward_pad_impl_f...\n");
+  sw_conv_backward_pad_impl_f(
