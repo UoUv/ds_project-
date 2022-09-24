ea57e3c0927e30b4f5bc9c957c1713b8bc648783
UoUv <107167004+UoUv@users.noreply.github.com>
2022-09-24 13:21:52 +0000

    set svn properties

--- unitest/src/test_im2col_zeropad_trans_batch.c
+++ unitest/src/test_im2col_zeropad_trans_batch.c
@@ -3,4 +3,13 @@
  * fang_jiarui@163,com
  * ****/
 #include "include/swim2col.h"
-#
+#include "include/swcommon.h"
+#include "./include/swtensortrans.h"
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "swblas.h"
+
+/******
+ * a 
