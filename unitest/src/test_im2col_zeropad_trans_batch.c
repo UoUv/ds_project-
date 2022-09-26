4223ac5df4ff32e6499068338a89752e20aea635
UoUv <107167004+UoUv@users.noreply.github.com>
2022-09-26 11:21:30 +0000

    Updated API models and rebuilt service gems.

--- unitest/src/test_im2col_zeropad_trans_batch.c
+++ unitest/src/test_im2col_zeropad_trans_batch.c
@@ -18,4 +18,6 @@
  * 2. zeropadding, (N, R, C, B) -> (K*K*N + pad, Ro*Co*B +pad), adding pad make GEMM easy
  * 3. batch-GEMM , make sure filters are like (K*K*Ni+pad, No)
  * ***/
-void test_im2col_zeropad_batch_trans_swblas_float(int channels, int filters, int hei
+void test_im2col_zeropad_batch_trans_swblas_float(int channels, int filters, int height, int width, int kernel_h, int kernel_w, int pad_h, int pad_w, int stride_h, int stride_w, int batch_size) {
+  printf("begin test_im2col_zeropad_batch_swblas_float\n");
+  int i, 
