0b6906f0e8d17123bb5509d0060c9c115ad74bb5
UoUv <107167004+UoUv@users.noreply.github.com>
2022-10-02 08:20:11 +0000

    Prepare 0.6.3 release

--- unitest/src/test_im2col_zeropad_trans_batch.c
+++ unitest/src/test_im2col_zeropad_trans_batch.c
@@ -33,4 +33,8 @@ void test_im2col_zeropad_batch_trans_swblas_float(int channels, int filters, int
   int im_size = channels*height*width;
   int col_size = output_w*output_h*channels*kernel_h*kernel_w;
   int zeropad_col_rowsize = (output_w * output_h + 127)/128*128;
-  int zeropad_col_colsize = (kernel_h
+  int zeropad_col_colsize = (kernel_h * kernel_w * channels + 7)/8*8;
+  int pad_col_size = zeropad_col_rowsize * zeropad_col_colsize;
+  int group_ = 1;
+
+  printf("forward: channels %d, filters %d, height %d, width %
