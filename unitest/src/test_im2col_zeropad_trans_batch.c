507b14fcf99f40c44cf5acaec590c403bf9ff5df
UoUv <107167004+UoUv@users.noreply.github.com>
2022-10-03 15:41:20 +0000

    Updates Documentation/Introduction.md

--- unitest/src/test_im2col_zeropad_trans_batch.c
+++ unitest/src/test_im2col_zeropad_trans_batch.c
@@ -41,4 +41,8 @@ void test_im2col_zeropad_batch_trans_swblas_float(int channels, int filters, int
       pad_h %d, pad_w %d, output_w %d, output_h %d, stride_h %d, stride_w %d, zeropad_col_rowsize %d (%d)\
       zeropad_col_colsize %d (%d), batch_size %d\n",
           channels, filters, height, width, \
-          kernel_h,kernel_w,pad_h,pad_w,output_w,output_h,stride_h,stride_w, zeropad_col_rowsize, out
+          kernel_h,kernel_w,pad_h,pad_w,output_w,output_h,stride_h,stride_w, zeropad_col_rowsize, output_w*output_h,zeropad_col_colsize,
+          kernel_h*kernel_w*channels,
+          batch_size);
+
+  if(batch_size*((width + 2*pad_w) + output_w)*sizeof(float) > 60*102
