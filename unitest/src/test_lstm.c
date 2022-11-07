5ea20735dfe2dc1036c23b86266fe8fd493e17d0
UoUv <107167004+UoUv@users.noreply.github.com>
2022-11-07 19:51:07 +0000

    Correções

--- unitest/src/test_lstm.c
+++ unitest/src/test_lstm.c
@@ -6,4 +6,11 @@
 #include <math.h>
 
 #define Dtype float
-Dtype sigmoid
+Dtype sigmoid(Dtype x) {
+  return 1. / (1. + exp(-x));
+}
+int test_lstm()
+{
+  int a[10],b[10],c[10];
+  a[0]=64;a[1]=128;a[2]=256;a[3]=512;
+  b[0]=128;b[1]=256;b[2]=512;b[3]=1024;b[4]=1600;
