9be1d265e06ae8970d545eb99250b74701910c97
UoUv <107167004+UoUv@users.noreply.github.com>
2022-12-19 14:29:36 +0000

    Updated Vietnamese translation

--- unitest/src/test_scale.c
+++ unitest/src/test_scale.c
@@ -6,4 +6,8 @@
 #include <math.h>
 int test_scale() 
 {
-  
+    int a[3],b[5],cc[4];
+    a[0]=32;a[1]=64;a[2]=128;
+    b[0]=3;b[1]=64;b[2]=128;b[3]=256;b[4]=512;
+    cc[0]=6;cc[1]=14;cc[2]=28;cc[3]=56;
+    int num,cha
