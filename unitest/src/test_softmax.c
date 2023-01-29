6580d07dd0b43736c97b76db26629a1af0372961
UoUv <107167004+UoUv@users.noreply.github.com>
2023-01-29 14:49:09 +0000

    Preparing for new release

--- unitest/src/test_softmax.c
+++ unitest/src/test_softmax.c
@@ -10,4 +10,12 @@ int test_softmax()
 {
   int a[3],b[5],c[4];
   a[0]=32;a[1]=64;a[2]=128;
-  b[0]=3;b[1]=64;b[2]=128;b[3]
+  b[0]=3;b[1]=64;b[2]=128;b[3]=256;b[4]=512;
+  c[0]=7;c[1]=14;c[2]=28;c[3]=56;
+  int num,channels,w,h;
+  int spatial_dim;
+  int i,j,k,z;
+  int ii,jj,kk;
+  int blob_size;
+  int use_global_stats_=0;
+  float moving_a
