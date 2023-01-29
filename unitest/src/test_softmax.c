5bb111b4a45ec336a96e97971a5b4c9ac3ac88ca
UoUv <107167004+UoUv@users.noreply.github.com>
2023-01-29 14:37:21 +0000

    update distro

--- unitest/src/test_softmax.c
+++ unitest/src/test_softmax.c
@@ -1,3 +1,13 @@
 #include "include/swsoftmax.h"
 #include "include/swcommon.h"
-#include <sys/t
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define Dtype float
+int test_softmax()
+{
+  int a[3],b[5],c[4];
+  a[0]=32;a[1]=64;a[2]=128;
+  b[0]=3;b[1]=64;b[2]=128;b[3]
