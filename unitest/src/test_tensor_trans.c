44903efde5596ee36f5e4af16dc60c5560e2a59d
UoUv <107167004+UoUv@users.noreply.github.com>
2023-03-17 11:47:20 +0000

    merge with head

--- unitest/src/test_tensor_trans.c
+++ unitest/src/test_tensor_trans.c
@@ -1,4 +1,12 @@
 #include "include/swtensortrans.h"
 #include "include/swcommon.h"
 #include <sys/time.h>
-#include <stdli
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "swblas.h"
+
+// high -> low
+// B, N, W, H
+inline int image_caffe_offset(int b, int n, int h, int w, int B, int N, int H, int W) {
+  return (((b*N + n)*H
